774ab005b9eac167a01e75f2da72a8828dda4b08
Pavel Zeman <pavel@pavel>
2011-12-29 21:12:35 +0100

    Modified checker to gather statistics

--- UUGoogleSyncChecker/src/cz/pavel/uugooglesync/checker/Checker.java
+++ UUGoogleSyncChecker/src/cz/pavel/uugooglesync/checker/Checker.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,9 +21,42 @@ public class Checker extends HttpServlet {
 	
 	private static final String SUPPORTED_IDS = "/SupportedIds.txt";
 	private static final String PARAMETER_NAME = "GOOGLE_ID";
+	
+	private static final int MAXIMUM_MAP_SIZE = 100;
+	
+	private static final Map<String, Integer> requestCounts = new HashMap<String, Integer>();
+
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		
+		log("Terminating servlet, following users were verified: ");
+		for (Map.Entry<String, Integer> entry : requestCounts.entrySet()) {
+			log(" " + entry.getKey() + " - " + entry.getValue());
+		}
+	}
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException  {
+		
 		String inputId = request.getParameter(PARAMETER_NAME);
+		log("Checking user " + inputId);
+		
+		// increase request count
+		synchronized (requestCounts) {
+			Integer requestCount = requestCounts.get(inputId);
+			if (requestCount == null) {
+				// if the map is too big, simply remove any element
+				if (requestCounts.size() >= MAXIMUM_MAP_SIZE) {
+					requestCounts.remove(requestCounts.keySet().iterator().next());
+				}
+				requestCount = Integer.valueOf(0);
+			}
+			requestCounts.put(inputId, Integer.valueOf(requestCount.intValue() + 1));
+		}
+
+		
 		URL url = Checker.class.getResource(SUPPORTED_IDS);
 		InputStream is = url.openStream();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
