b7af849656414741d83ac9b65822dec084f34322
Pavel Zeman <pavel@pavel>
2012-02-06 23:51:42 +0100

    ISS14 - Modified parsing of item place, it is now optional

--- UUGoogleSync/src/cz/pavel/uugooglesync/utils/HtmlParser.java
+++ UUGoogleSync/src/cz/pavel/uugooglesync/utils/HtmlParser.java
@@ -39,12 +39,38 @@ public class HtmlParser {
 		return result.toString();
 	}
 	
+	/**
+	 * Just a wrapper for {@link #extractRegExp(String, String, boolean, boolean)}.
+	 */
 	public static String extractRegExp(String data, String regexp) {
 		return extractRegExp(data, regexp, false);
 		
 	}
-	
+
+	/**
+	 * Just a wrapper for {@link #extractRegExp(String, String, boolean, boolean)}.
+	 */
 	public static String extractRegExp(String data, String regexp, boolean decode) {
+		return extractRegExp(data, regexp, decode, true);
+		
+	}
+
+	
+	/**
+	 * Extracts value from <tt>data</tt> using a regular expression (<tt>regexp</tt>) and
+	 * returns first match of the first group.
+	 * 
+	 * If there is no match found, it either returns empty string (if <tt>required</tt> is false) 
+	 * or throws {@link RuntimeException} (if <tt>required</tt> is true).
+	 * 
+	 * @param data string to search
+	 * @param regexp regular expression to search for 
+	 * @param decode if true, replaces HTML entities in the resulting data
+	 * @param required indicates, whether a match must be found
+	 * @return First match of the first group of the input regular expression, or 
+	 * empty string ("") if no match is found and <tt>required</tt> is false.
+	 */
+	public static String extractRegExp(String data, String regexp, boolean decode, boolean required) {
 		Pattern pattern = compiledPatterns.get(regexp);
 		if (pattern == null) {
 			pattern = Pattern.compile("(?i)" + regexp);
@@ -55,7 +81,7 @@ public class HtmlParser {
 		if (decode) {
 			result = result.replace("&amp;", "&");
 		}
-		if (result == null || result.length() == 0) {
+		if (required && (result == null || result.length() == 0)) {
 			throw new RuntimeException("Cannot extract regular expression " + regexp);
 		}
 		return result;
