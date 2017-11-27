d1515c3c6cc201862dea8fd964845e31e990b653
Pavel Zeman <pavel@pavel.cz>
2017-11-27 10:19:24 +0100

    Removed user ID verification, because the verification server does not exist any more

--- UUGoogleSync/src/cz/pavel/uugooglesync/google/GoogleManager.java
+++ UUGoogleSync/src/cz/pavel/uugooglesync/google/GoogleManager.java
@@ -1,19 +1,9 @@
 package cz.pavel.uugooglesync.google;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
 import org.apache.log4j.Logger;
 
 import com.google.api.client.googleapis.auth.oauth2.draft10.GoogleAccessProtectedResource;
@@ -21,16 +11,12 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson.JacksonFactory;
 import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.model.CalendarList;
-import com.google.api.services.calendar.model.CalendarListEntry;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.EventExtendedProperties;
 import com.google.api.services.calendar.model.Events;
 
 import cz.pavel.uugooglesync.utils.CalendarUtils;
 import cz.pavel.uugooglesync.utils.Configuration;
-import cz.pavel.uugooglesync.utils.HtmlParser;
-import cz.pavel.uugooglesync.utils.HttpClientUtils;
 import cz.pavel.uugooglesync.utils.LogUtils;
 
 public class GoogleManager {
@@ -47,28 +33,6 @@ public class GoogleManager {
 	
 	private Calendar service;
 	private String calendarId;
-	
-	private static final String VERIFICATION_SERVICE_URL = "http://kdkscarab.unicorn.cz/UUGoogleSyncChecker/Checker";
-	
-	private void verifyUserId(String userId) throws IOException {
-		log.info("Verifying user " + userId);
-		
-		DefaultHttpClient httpclient = HttpClientUtils.getHttpClient();
-        HttpPost httpost = new HttpPost(VERIFICATION_SERVICE_URL);
-        
-        List <NameValuePair> nvps = new ArrayList <NameValuePair>();
-        nvps.add(new BasicNameValuePair("GOOGLE_ID", userId));
-        httpost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
-        
-        HttpResponse response = httpclient.execute(httpost);
-        HttpEntity entity = response.getEntity();
-        String result = HtmlParser.getContents(entity.getContent());
-        if (!"1".equals(result)) {
-        	throw new RuntimeException("User " + userId + " not authorized to run this application");
-        }
-	}
-	
-	
 	
 	public Map<String, Event> getEvents(java.util.Calendar startDate, java.util.Calendar endDate) throws IOException {
 	    HttpTransport httpTransport = new NetHttpTransport();
@@ -92,18 +56,6 @@ public class GoogleManager {
 	        .setApplicationName("UISGoogleSync")
 	        .setHttpRequestInitializer(accessProtectedResource)
 	        .build();
-	    
-	    // get user id from the calendar list
-	    CalendarList calendarList = service.calendarList().list().execute();
-	    String userId = "";
-	    for (CalendarListEntry calendarListEntry : calendarList.getItems()) {
-	    	String id = calendarListEntry.getId();
-	    	if (id.endsWith("gmail.com")) {
-	    		userId = id;
-	    		break;
-	    	}
-	    }
-	    verifyUserId(userId);
 	    
 	    // get events
 	    log.debug("Loading events from " + CalendarUtils.calendarToGoogleString(startDate) + " to " + CalendarUtils.calendarToGoogleString(endDate));
